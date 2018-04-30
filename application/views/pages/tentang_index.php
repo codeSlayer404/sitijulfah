4036855f25d3fe66368b610014e92d17e1b4aba2
Fajar <jars712.fs@gmail.com>
2018-04-30 15:07:23 +0700

    Initial commit

---
+++ application/views/pages/tentang_index.php
@@ -0,0 +1,7 @@
+<div class="card card-default" style="border-radius: 0px;margin: 20px 0;">
+   
+     <div class="card-header">Tentang Pembuat Website</div>
+     <div class="card-body">
+          <p>Perkenalkan Nama saya Siti Julfah kelas XI-RPL2 . <br/>Tujuan pembuatan website ini untuk memenuhi tugas Web Dinamis XI materi CRUD (Create, Read, Update & Delete).</p>
+     </div>
+</div>
